9c6a0e78f0c391907f167781e72e1c33a88267e2
GillesStyns <gilles.styns@student.howest.be>
2017-03-28 13:45:54 +0200

    made adjustments to index id's added certain css made js hide/show functions (can be built on when later functions have to be added)

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -3,5 +3,78 @@
  */
 
 $(document).ready(function () {
-
+    beginScherm();
+    $('#enterName').on('submit', gaVerderVanStartScherm);
+    $('#startGame').on('submit', startTheGame);
+    $('#controleer').on('click', controleerOplossing);
+    $('#Continue').on('click', continueNaControle);
+    $('#Restart').on('click', restartNaControle);
+    $('#Finish').on('click', eindeSpelBijJuisteOplossing);
+    $('#viewHighscores').on('click', viewHighscores);
+    $('#Restart2').on('click', restartNaEindeSpel);
+    $('#viewSolution').on('click', viewSolution);
+    $('#goBackFromSolution').on('click', goBackFromSolution);
 });
+
+var beginScherm = function () {
+    $('#selectDifficulty').hide();
+    $('#SudokuGame').hide();
+    $('#Congratulations').hide();
+    $('#Solution').hide();
+};
+
+var gaVerderVanStartScherm = function (e) {
+    e.preventDefault();
+    $('#HoofdMenu').hide();
+    $('#selectDifficulty').show();
+};
+
+var startTheGame = function (e) {
+    e.preventDefault();
+    $('#selectDifficulty').hide();
+    $('#SudokuGame').show();
+    $('#Controle').hide();
+};
+
+var controleerOplossing =function() {
+    $('#Controle').show();
+    $('#theGame').hide();
+};
+
+var continueNaControle = function () {
+    $('#Controle').hide();
+    $('#theGame').show();
+};
+
+var restartNaControle = function () {
+    $('#Controle').hide();
+    $('#theGame').show();
+};
+
+var eindeSpelBijJuisteOplossing = function () {
+    $('#SudokuGame').hide();
+    $('#Congratulations').show();
+    $('#HighScores').hide();
+};
+
+var viewHighscores = function () {
+    $('#Einde').hide();
+    $('#viewHighscores').show();
+};
+
+var restartNaEindeSpel = function () {
+    beginScherm();
+    $('#HoofdMenu').show();
+};
+
+var viewSolution = function () {
+    $('#Congratulations').hide();
+    $('#Solution').show();
+}
+
+var goBackFromSolution = function () {
+    $('#Congratulations').show();
+    $('#Solution').hide();
+    $('#HighScores').hide();
+    $('#Einde').show();
+};
